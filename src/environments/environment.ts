b8023a2390a7d4f15341beadbfadbbe9ff137c12
Beatriz Silva <beatrizxte@gmail.com>
2023-09-22 15:51:58 -0300

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false, 
+
+    firebase: {
+        apiKey: "AIzaSyBVBGqjHfYEaEISNmfhcpie5cOtma3-CrI",
+        authDomain: "iontest-9ae40.firebaseapp.com",
+        projectId: "iontest-9ae40",
+        storageBucket: "iontest-9ae40.appspot.com",
+        messagingSenderId: "599568729095",
+        appId: "1:599568729095:web:583e73020b4483e83896a2"
+    }
+
+};
